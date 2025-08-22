f3fb129ea6c6b040b92bc2a7b7675499ac5bb269
Chanzuderrick <137392204+Chanzuderrick@users.noreply.github.com>
2025-08-23 00:36:12 +0300

    changes commit

--- interpolationSearch1.java
+++ interpolationSearch1.java
@@ -1,4 +1,26 @@
-public class interpolationSearch1{
+public class interpolationSearch1 {
+    
+    private static int interpolationSearch(int[] array, int value) {
+        int high = array.length - 1;
+        int low = 0;
+        
+        while (value >= array[low] && value <= array[high] && low <= high) {
+            int probe = (high - low) * (value - array[low]) / (array[high] - array[low]);
+            System.out.println("probe:   " + probe);
+
+            if (array[probe] == value) {
+            return probe;
+        } else if (array[probe] < value) {
+            low = probe + 1;
+
+        } else {
+            high = probe - 1;
+        }
+
+        }
+        return -1;
+        
+    }
     public static void main(String[] args){
         /*
          * INTERPOLATION SEARCH
@@ -9,5 +31,16 @@ public class interpolationSearch1{
          * average cases: 0(log(log (n)))
          * worst cases: 0(n) [values increase exponentially]
         */
+
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int index = interpolationSearch(array, 8);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+
+    
     }
 }
