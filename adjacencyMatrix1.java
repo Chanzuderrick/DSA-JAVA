2fa24c980eabadbb78ed91b98ce47731e6d529e7
Chanzuderrick <137392204+Chanzuderrick@users.noreply.github.com>
2025-09-01 18:45:37 +0300

    CHANGES COMMIT

---
+++ adjacencyMatrix1.java
@@ -0,0 +1,16 @@
+public class adjacencyMatrix1 {
+    public static void main(String[] args) {
+        /*
+         * ADJACENCY MATRIX
+         * is a 2D array to store 1 and 0 to represent edges
+         * number of rows = number of unique nodes
+         * number of columns = number of unique nodes
+         * runtime compexity to check an Edge: 0(1)
+         * space complexity 0(v^2)
+         * 
+        */
+
+        
+    }
+    
+}
