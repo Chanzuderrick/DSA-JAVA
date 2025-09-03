1142f3018af0523f35e5e0183de73b47cda3823a
Chanzuderrick <137392204+Chanzuderrick@users.noreply.github.com>
2025-09-03 14:22:52 +0300

    dfs changes commit

---
+++ DepthFirstSearch1.java
@@ -0,0 +1,24 @@
+public class DepthFirstSearch1 {
+    /*
+     * DEPTH FIRST SEARCH
+     * its a search algorithm for traversing a tree or a graph data structure
+     * we can break this into three steps
+     * 1. pick a route
+     * 2. keep going until you reach a dead end, or a previously visited node
+     * 3. backtrack to last node that has unvisited adjacent neighbours
+     * 
+     * one way we can keep track of our position is to use a stack
+     * we can use a call stack
+     * whenever we visit a node we will push it to the stack
+     * if we are unsuccessful we will use step 3 by popping the elements in the stack till we reach the first element
+     * then we begin the process again till we are successful
+     * 
+     * 
+     * 
+    */
+    public static void main(String[]args){
+        System.out.println("Notes on depth first search algorithm");
+
+    }
+    
+}
