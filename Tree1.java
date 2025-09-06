d461a2e20d80e6109d04ef3429797447483832da
Chanzuderrick <137392204+Chanzuderrick@users.noreply.github.com>
2025-09-06 20:06:35 +0300

    changes commit

---
+++ Tree1.java
@@ -0,0 +1,29 @@
+public class Tree1{
+    public static void main(String[] args){
+        /*
+         * TREES 
+         * non linear data structures where nodes are organized in a hierachy
+         * file explorer
+         * databases
+         * dns
+         * HTML DOM
+         * top of the tree is known as the root node. it has no incoming edges
+         * any nodes at the bottom of the tree are called leaf nodes. dont have outgoing edges
+         * any nodes in the middle are called branch. they have incoming and outgoing nodes.
+         * Nodes with outgoing edges are known as parents
+         * Nodes with incoming edges are known as child nodes
+         * nodes can be parent/children
+         * any two nodes that share a common parent are known as siblings
+         * subtree is a smaller tree within a larger tree
+         * size of a tree is equal to the number of nodes
+         * depth: number of edges below the root node
+         * height: number of edges above the furthest node
+         * 
+         * 
+         * 
+        */
+
+        System.out.println("Introduction to trees notes");
+    }
+
+}
