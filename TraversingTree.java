ef54a9bffbc1444772ca7cb472484c8a53569917
Chanzuderrick <137392204+Chanzuderrick@users.noreply.github.com>
2025-09-09 14:39:54 +0300

    changes commit

---
+++ TraversingTree.java
@@ -0,0 +1,37 @@
+public class TraversingTree{
+    public static void main(String[] args){
+
+        //NOTES ARE BASED ON BINARY TREE
+
+        /*
+         * We are going to talk about inorder, postorder and pre order
+         * we begin at the root of the tree in all of the methods
+         * 
+         * 
+         * we will have a class like this in all methods
+         * 
+         * public class Node{
+         * Node left;
+         * Node right;
+         * int data;
+         * 
+         * Node(int data){
+         * this.data = data;
+         * }
+         * }
+         * 
+         * INORDER TRAVERSAL
+         * 
+         * lets begin with an inorder traversal
+         * LEFT -> ROOT -> RIGHT
+         * 
+         * private void traverseTree(Node root){
+         * traverseTree(root.left);
+         * System.out.print(root.data);
+         * traverseTree(root.right);
+         * }
+         * 
+        */
+
+    }
+}
